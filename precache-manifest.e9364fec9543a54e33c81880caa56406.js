5c54af09571791d9afd3f0aa8ed6cf8e299b59bc
Artemij Fedosejev <artemij.fedosejev@gmail.com>
2020-04-12 03:54:04 +0100

    Updates

---
+++ precache-manifest.e9364fec9543a54e33c81880caa56406.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f8f511f0ea7f4ced68c756341a1e8cf",
+    "url": "/react-shopping-cart/index.html"
+  },
+  {
+    "revision": "642e2db3dc81e737266b",
+    "url": "/react-shopping-cart/static/js/2.ff38876e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-shopping-cart/static/js/2.ff38876e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16bd3c29e2542c2303b7",
+    "url": "/react-shopping-cart/static/js/main.c473bcb8.chunk.js"
+  },
+  {
+    "revision": "be39af72a6ae7697d5bc",
+    "url": "/react-shopping-cart/static/js/runtime-main.85c54643.js"
+  }
+]);
